2eae13b912781574be62ed7c0acb52723c00ca03
Preston Wallace <wallace.preston@gmail.com>
2023-01-23 14:49:54 -0800

    instructions for vars and consts

--- 01/1-primers/1-variables-constants/index.js
+++ 01/1-primers/1-variables-constants/index.js
@@ -1,4 +1,8 @@
-// define a new variable called `age` and set it equal to your age
+// define a new variable called `age` and set it equal to your age. 
+// Remember, it is a variable, so it should be able to be redefined.
+
+// define a new constant called `name` and set it equal to your name.
+// Remember, it is a constant, so it should not be able to be redefined.
 
 export default {
   age,
