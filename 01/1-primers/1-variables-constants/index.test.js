7b2c81884d7c454b4daae6daaca400228402b894
Preston Wallace <wallace.preston@gmail.com>
2023-01-23 14:48:23 -0800

    tests for variables and constants

--- 01/1-primers/1-variables-constants/index.test.js
+++ 01/1-primers/1-variables-constants/index.test.js
@@ -7,9 +7,30 @@
 import { default as variables } from './index.js';
 const { age } = variables;
 
+// test for age variable
 describe('age', () => {
+  // confirms data type
   it('should be a number', () => {
     expect(typeof age).toBe('number');
   });
+  // confirms the variable can be redefined
+  it('should be able to be redefined', () => {
+    age = 10;
+    expect(age).toBe(10);
+  });
 });
+
+// test for name constant
+describe('name', () => {
+  // confirms data type
+  it('should be a string', () => {
+    expect(typeof name).toBe('string');
+  });
+  // confirms the constant cannot be redefined
+  it('should not be able to be redefined', () => {
+    expect(() => {
+      name = 'bob';
+    }).toThrow();
+  });
+}); 
 
