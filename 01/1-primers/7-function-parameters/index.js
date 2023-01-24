9bac2a071a47150c6013db7b373ab281b7e064ce
Preston Wallace <wallace.preston@gmail.com>
2023-01-24 07:27:47 -0800

    add function parameters challenges

--- 01/1-primers/7-function-parameters/index.js
+++ 01/1-primers/7-function-parameters/index.js
@@ -1,6 +1,61 @@
-// create a new function expression called `subtract` that takes two numbers and returns the difference between them
+// declare a function called `subtract` that takes two numbers and returns the difference between them
+// example: subtract(5, 2) should return 3
+
+
+// declare a function called `sum` that takes two numbers and returns the sum of them
+// example: sum(1, 2) should return 3
+
+// declare a function called `convert` that takes an integer `minutes` and converts it to seconds.
+// examples: 
+// convert(5) -> 300
+// convert(3) -> 180
+// convert(2) -> 120
+
+// declare a function called `addition` that takes a number as an argument, increments the number by +1 and returns the result.
+// examples:
+// addition(0) -> 1
+// addition(9) -> 10
+// addition(-3) -> -2
+
+// declare a function called `concatenate` that takes two strings and returns the concatenation of the two strings.
+// examples:
+// concatenate("a", "b") -> "ab"
+// concatenate("code", "up") -> "codeup"
+// concatenate("connect", "four") -> "connectfour"
+
+// declare a function called 'isEqual' that takes two arguments and returns true if the two arguments are equal. Make sure they are strictly equal.
+// examples:
+// isEqual(1, 1) -> true
+// isEqual(1, 2) -> false
+// isEqual("1", 1) -> false
+// isEqual("1", "1") -> true
+// isEqual("billy", "billy") -> true
+// isEqual("billy", "bill") -> false
+
+// declare a function called `max` that takes three numbers and returns the largest number.
+// examples:
+// max(1, 2, 3) -> 3
+// max(5, 3, 1) -> 5
+// max(5, 5, 5) -> 5
+// max(5, 5, 1) -> 5
+
+
+// declare a function called `isEmpty` that takes a string and returns true if the string is empty. Otherwise, returns false.
+// examples:
+// isEmpty("") -> true
+// isEmpty(" ") -> false
+// isEmpty("a") -> false
+
+
 
 
 export default {
   subtract,
+  sum,
+  convert,
+  addition,
+  concatenate,
+  isEqual,
+  max,
+  isEmpty,
 }
--- 01/1-primers/7-function-parameters/index.test.js
+++ 01/1-primers/7-function-parameters/index.test.js
@@ -1,5 +1,14 @@
 import { default as variables } from './index.js';
-const { subtract } = variables;
+const {
+  subtract,
+  sum,
+  convert,
+  addition,
+  concatenate,
+  isEqual,
+  max,
+  isEmpty,
+} = variables;
 
 describe('subtract', () => {
   it('should be a function', () => {
@@ -19,4 +28,63 @@ describe('subtract', () => {
   });
 
 });
+
+describe("sum", () => {
+  test("should return the sum of two numbers", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
+
+describe("convert", () => {
+  test("should convert minutes to seconds", () => {
+    expect(convert(5)).toBe(300);
+    expect(convert(3)).toBe(180);
+    expect(convert(2)).toBe(120);
+  });
+});
+
+describe("addition", () => {
+  test("should increment the number by +1 and return the result", () => {
+    expect(addition(0)).toBe(1);
+    expect(addition(9)).toBe(10);
+    expect(addition(-3)).toBe(-2);
+  });
+});
+
+describe("concatenate", () => {
+  test("should return the concatenation of two strings", () => {
+    expect(concatenate("a", "b")).toBe("ab");
+    expect(concatenate("code", "up")).toBe("codeup");
+    expect(concatenate("connect", "four")).toBe("connectfour");
+  });
+});
+
+describe("isEqual", () => {
+  test("should return true if the two arguments are strictly equal", () => {
+    expect(isEqual(1, 1)).toBe(true);
+    expect(isEqual(1, 2)).toBe(false);
+    expect(isEqual("1", 1)).toBe(false);
+    expect(isEqual("1", "1")).toBe(true);
+    expect(isEqual("billy", "billy")).toBe(true);
+    expect(isEqual("billy", "bill")).toBe(false);
+  });
+});
+
+describe("max", () => {
+  test("should return the largest number of three numbers", () => {
+    expect(max(1, 2, 3)).toBe(3);
+    expect(max(5, 3, 1)).toBe(5);
+    expect(max(5, 5, 5)).toBe(5);
+    expect(max(5, 5, 1)).toBe(5);
+  });
+});
+
+describe("isEmpty", () => {
+  test("should return true if the string is empty, otherwise returns false", () => {
+    expect(isEmpty("")).toBe(true);
+    expect(isEmpty(" ")).toBe(false);
+    expect(isEmpty("a")).toBe(false);
+  });
+});
+
 
