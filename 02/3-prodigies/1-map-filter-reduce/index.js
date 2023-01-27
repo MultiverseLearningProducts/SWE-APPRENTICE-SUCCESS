d95f7561c3b9d33e61127761fbb295f0e3e7165f
stevenjlance <steven.j.lance@gmail.com>
2023-01-27 11:44:41 -0500

    map, filter, reduce tests and problems created

---
+++ 02/3-prodigies/1-map-filter-reduce/index.js
@@ -0,0 +1,121 @@
+// 1. An array called nums has been created. Update the doubles variable with an array that contains all values in the nums array multiplied by 2. Use a map function to solve this problem.
+let nums = [3, 10, 1, 25];
+
+let doubles;
+
+
+// 2. An array called bigNumbers has been created. Update the smallNums variable with the values in bigNumbers divided by 2.
+let bigNumbers = [100, 200, 300, 1000, 525600]
+
+let smallNums;
+
+
+// 3. An array of names has been provided. Update the coders variable with the name and the string "is a coder" added to the end of the string"
+/*
+Example:
+let name = ["Becky", "Luther"];
+
+After the map function coders would look like:
+coders = ["Becky is a coder", "Luther is a coder"];
+*/
+let names = ['Yadira', 'Kadiatou', 'Isaiah', 'Fancisco', 'Darius'];
+
+let coders;
+
+
+// 4. The variables evens and odds have been initialized. Using a filter function, update each with an array of even and odd values stored in the oneToTen array.
+let oneToTen = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let evens;
+let odds;
+
+
+
+// Use the following array for questions 5 and 6
+let superheros = ["Black Panther", "Wonder Woman", "Captain Marvel", "Spider Man", "Thor"]
+
+// 5. Use a filter function to update the variable longNames with an array of all superheros that have names longer than 10 characters.
+let longNames;
+
+
+
+// 6. Update the variable containsC with an array of superheros that have at least one c in their name. This should account for both uppercase and lowercase "c".
+let containsC;
+
+
+
+// 7. An array of numbers has been stored in sumThis. Sum all the values and store the final value in the total variable.  
+let sumThis = [7, 1, 3, 5, 6, 2, 8, 10, 0, 4, 9];
+
+let total;
+
+
+
+// 8. An array called epic has been created for you. Update the story variable so it contains a single string with all the words combined together.
+/*
+Example:
+Final output => "a long time ago in a galaxy far far away"
+*/
+let epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+
+let story;
+
+
+
+// Use the following for questions 9 and 10
+const people = [
+	{name: 'Yadira', age: 22, nickname: 'Deary', 'Favorite Animal': 'Cat', 'Birth Place': 'Queens', superhero: "Black Panther" }, 
+	{name: 'Kadiatou', age: 28,nickname: 'Kadi-B','Favorite Animal': 'Ferret','Birth Place': 'Bronx', superhero: "Wonder Woman"}, 
+	{name: 'Isaiah', age: 27,nickname: 'Izzy','Favorite Animal': 'Dog','Birth Place': 'Manhattan', superhero: "Captain Marvel"},
+	{name: 'Fancisco', age: 33,nickname: 'Paco','Favorite Animal': 'Sloth','Birth Place': 'Bronx', superhero: "Spider Man"},
+	{name: 'Darius', age: 29,nickname: 'DRock','Favorite Animal': 'Zebra','Birth Place': 'Brooklyn', superhero: "Thor"}
+]
+
+// 9. Filter the people array to be an array of people that are older than 25. Store the value in the variable oldPeople.
+
+let oldPeople;
+
+
+
+// 10. We need an object that contains the name as a key and the nickname as a value. Store the value in the variable nicknames.
+/*
+Example:
+{
+    "Yadira": "Deary",
+    "Kadiatou": "Kadi-B",
+    ...
+}
+*/
+
+let nicknames;
+
+
+
+
+// 11. An array of objects containing students and test scores has been stored in the students variable. In the variable highPerformers, store only those students that have an average score greater than or equal to a 90.
+const students = [
+  { name: "Alice", scores: [90, 85, 92] },
+  { name: "Bob", scores: [75, 80, 85] },
+  { name: "Charlie", scores: [90, 95, 85] },
+  { name: "David", scores: [100, 100, 100] }
+];
+
+let highPerformers;
+
+
+
+
+// DO NOT EDIT BELOW THIS LINE
+export default { 
+    doubles,
+    smallNums,
+    coders,
+    evens,
+    odds,
+    longNames,
+    containsC,
+    total,
+    story,
+    oldPeople,
+    nicknames,
+    highPerformers
+};
---
+++ 02/3-prodigies/1-map-filter-reduce/index.test.js
@@ -0,0 +1,136 @@
+import { default as variables } from './index.js';
+
+const { 
+    doubles,
+    smallNums,
+    coders,
+    evens,
+    odds,
+    longNames,
+    containsC,
+    total,
+    story,
+    oldPeople,
+    nicknames,
+    highPerformers
+} = variables;
+
+describe("doubles tests", () => {
+    let nums = [3, 10, 1, 25];
+    test("doubles is an array", () => {
+        expect(Array.isArray(doubles)).toBe(true);
+    })
+
+    test("doubles array returns array that is double nums", () => {
+        expect(doubles).toEqual([6, 20, 2, 50]);
+    })
+})
+
+describe("smallNums tests", () => {
+    let bigNumbers = [100, 200, 300, 1000, 525600]
+    test("smallNums is an array", () => {
+        expect(Array.isArray(smallNums)).toBe(true);
+    })
+
+    test("smallNums array returns array that contains values that are half the bigNumbers array", () => {
+        expect(smallNums).toEqual([50, 100, 150, 500, 262800]);
+    })
+})
+
+describe("coders tests", () => {
+    let names = ['Yadira', 'Kadiatou', 'Isaiah', 'Fancisco', 'Darius'];
+    test("coders is an array", () => {
+        expect(Array.isArray(coders)).toEqual(true);
+    })
+
+    test("coders contains 'is a coder' added to each name", () => {
+        expect(coders).toEqual(['Yadira is a coder', 'Kadiatou is a coder', 'Isaiah is a coder', 'Fancisco is a coder', 'Darius is a coder']);
+    })
+})
+
+describe("evens and odds tests", () => {
+    test("both evens and odds are arrays", () => {
+        expect(Array.isArray(evens)).toEqual(true);
+        expect(Array.isArray(odds)).toEqual(true);
+    })
+
+    test("evens contains only even values", () => {
+        expect(evens.sort()).toEqual([0, 2, 4, 6, 8, 10].sort());
+        expect(odds.sort()).toEqual([1, 3, 5, 7, 9].sort());
+    })
+})
+
+describe("superheros tests", () => {
+    test("both longNames and containsC are arrays", () => {
+        expect(Array.isArray(longNames)).toEqual(true);
+        expect(Array.isArray(containsC)).toEqual(true);
+    })
+
+    test("longNames contains only superheros that have more than 10 characters in their name", () => {
+        expect(longNames.sort()).toEqual([ 'Black Panther', 'Wonder Woman', 'Captain Marvel'].sort());
+    })
+
+    test("contains C contains only superheros with c in their name (case insensitive", () => {
+        expect(containsC.sort()).toEqual([ 'Black Panther', 'Captain Marvel' ].sort());
+    })
+})
+
+describe("total reducer tests", () => {
+    test("total contains a number", () => {
+        expect(typeof total).toEqual('number');
+    })
+
+    test("total contains the correct value", () => {
+        expect(total).toEqual(55);
+    })
+})
+
+describe("story reducer tests", () => {
+    test("story contains a string", () => {
+        expect(typeof story).toEqual('string');
+    })
+
+    test("story contains the correct value", () => {
+        expect(story).toEqual("a long time ago in a galaxy far far away");
+    })
+})
+
+describe("oldPeople  tests", () => {
+    test("oldPeople is an array", () => {
+        expect(Array.isArray(oldPeople)).toEqual(true);
+    })
+
+    test("old people contains only people older than 25", () => {
+        expect(oldPeople[0].age > 25).toBe(true);
+        expect(oldPeople[1].age > 25).toBe(true);
+        expect(oldPeople[2].age > 25).toBe(true);
+    })
+})
+
+
+describe("nicknames tests", () => {
+    test("nicknames is an object", () => {
+        expect(typeof nicknames).toEqual('object');
+    })
+
+    test("nicknames contains key value pairs of name/nickname", () => {
+        expect(nicknames).toEqual(
+        {
+          Yadira: 'Deary',
+          Kadiatou: 'Kadi-B',
+          Isaiah: 'Izzy',
+          Fancisco: 'Paco',
+          Darius: 'DRock'
+        });
+    })
+})
+
+describe("highPerformers tests", () => {
+    test("highPerformers is an array", () => {
+        expect(Array.isArray(highPerformers)).toEqual(true);
+    })
+
+    test("highPerformers has values only student(s) with averages above 90", () => {
+        expect(highPerformers[0].name === "Charlie" || highPerformers[0].name === "David").toEqual(true)
+    })
+})
