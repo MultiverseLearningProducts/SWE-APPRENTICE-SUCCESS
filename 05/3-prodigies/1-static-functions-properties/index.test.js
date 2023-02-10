7b1d09de07231f91ded4cf2c07e912d24ee5b30a
Preston Wallace <wallace.preston@gmail.com>
2023-02-10 13:07:46 -0800

    move inheritance challenges into static functions challenges

--- 05/3-prodigies/1-static-functions-properties/index.test.js
+++ 05/3-prodigies/1-static-functions-properties/index.test.js
@@ -1,4 +1,4 @@
-import { Chuckles } from "./index.js";
+import { Chuckles, Character, Party } from "./index.js";
 
 describe("Chuckles class", () => {
   let chuckles;
@@ -45,5 +45,49 @@ describe("Chuckles class", () => {
     chuckles.addJoke("Why did the tomato turn red? Because it saw the salad dressing!");
     chuckles.addJoke("Why did the scarecrow win an award? Because he was outstanding in his field!");
     expect(chuckles.jokeCount()).toEqual(2);
+  });
+});
+
+
+describe("Party class", () => {
+  let leader = new Character("Jane Doe");
+
+  it("increments the static property totalParties when a party is created", () => {
+    const leader = new Character("Uncle Sam");
+    const party = new Party(leader);
+    expect(Party.totalParties).toBe(1);
+  });
+
+  it("throws an error when no leader is provided", () => {
+    expect(() => {
+      new Party();
+    }).toThrowError("A party needs a leader!");
+  });
+
+  it("throws an error when leader is already leading a party", () => {
+    const leader = new Character("Jane Doe");
+    const party1 = new Party(leader);
+    expect(() => {
+      new Party(leader);
+    }).toThrowError("Jane Doe is already leading a party.");
+  });
+
+  it("sets the leader property with the leader's name", () => {
+    const leader = new Character("Bob Smith");
+    const party = new Party(leader);
+    expect(party.leader).toBe(leader.name);
+  });
+
+  it("has a members property that is an array with the first element being the leader object", () => {
+    const leader = new Character("Barnaby Jones");
+    const party = new Party(leader);
+    expect(Array.isArray(party.members)).toBe(true);
+    expect(party.members[0]).toBe(leader);
+  });
+
+  it("adds the leader's name to the static property currentLeaders when a party is created", () => {
+    const leader = new Character("Oliver Queen");
+    const party = new Party(leader);
+    expect(Party.currentLeaders[Party.currentLeaders.length - 1]).toEqual(leader.name);
   });
 });
