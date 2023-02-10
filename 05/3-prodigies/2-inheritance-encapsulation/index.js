2959117e8255eaf455911d1aae0be39be9a0a210
Preston Wallace <wallace.preston@gmail.com>
2023-02-10 13:31:01 -0800

    edit prompt

--- 05/3-prodigies/2-inheritance-encapsulation/index.js
+++ 05/3-prodigies/2-inheritance-encapsulation/index.js
@@ -1,6 +1,6 @@
 /* 
 
-The Great Galactic Pie Eating Contest
+Welcome to the Great Galactic Pie Eating Contest! This competition is like no other, as it will test the stomach capacity and speed of pie eating for all contestants. With the most delicious pies from all around the galaxy, this will be a showdown for the ages. Who will emerge as the champion and take home the title of the galaxy's top pie eater? Read on to find out!
 
 Contestant Class
 Create a `Contestant` class with the following properties:
@@ -19,7 +19,7 @@ And the following methods:
 - eatPie(amount): if pieEaten plus amount is less than or equal to 20, calls the eatPie method from the Contestant class, and adds the amount to the pieEatingRecords array.
 - setWinningPies(amount): sets the winningPies property to the amount.
 
-Have fun and may the best chef win!
+Have fun and may the best pie eater win!
 
 */
 
