4b188b333fc2939dd6219437cc28fc635cb5c45f
Preston Wallace <wallace.preston@gmail.com>
2023-02-10 12:30:25 -0800

    static functions properties classes challenges

---
+++ 05/3-prodigies/1-static-functions-properties/index.js
@@ -0,0 +1,21 @@
+/* 
+Create a class called Chuckles with the following methods:
+
+Instance Properties
+- jokes: an array that will contain all the jokes that are added to the class
+- punchline: a string that will store the current punchline of the joke
+
+Instance Methods
+- addJoke(joke): takes in a string as an argument and pushes it to the jokes array
+- getRandomJoke(): returns a random joke from the jokes array
+- setPunchline(line): takes in a string as an argument and sets it as the punchline
+- getPunchline(): returns the punchline
+
+Static Methods
+- makeEveryoneLaugh(): returns a string "LOL"
+- jokeCount(): returns the number of jokes in the jokes array
+*/
+
+export class Chuckles {
+
+}
---
+++ 05/3-prodigies/1-static-functions-properties/index.test.js
@@ -0,0 +1,49 @@
+import { Chuckles } from "./index.js";
+
+describe("Chuckles class", () => {
+  let chuckles;
+
+  beforeEach(() => {
+    chuckles = new Chuckles();
+  });
+
+  it("should have an empty jokes array on initialization", () => {
+    expect(chuckles.jokes).toEqual([]);
+  });
+
+  it("should have a punchline property that is an empty string on initialization", () => {
+    expect(chuckles.punchline).toEqual("");
+  });
+
+  it("should be able to add a joke", () => {
+    chuckles.addJoke("Why did the tomato turn red? Because it saw the salad dressing!");
+    expect(chuckles.jokes.length).toEqual(1);
+  });
+
+  it("should be able to return a random joke", () => {
+    chuckles.addJoke("Why did the tomato turn red? Because it saw the salad dressing!");
+    chuckles.addJoke("Why did the scarecrow win an award? Because he was outstanding in his field!");
+    const joke = chuckles.getRandomJoke();
+    expect(chuckles.jokes).toContain(joke);
+  });
+
+  it("should be able to set the punchline", () => {
+    chuckles.setPunchline("Because it was the life of the party!");
+    expect(chuckles.punchline).toEqual("Because it was the life of the party!");
+  });
+
+  it("should be able to get the punchline", () => {
+    chuckles.setPunchline("Because it was the life of the party!");
+    expect(chuckles.getPunchline()).toEqual("Because it was the life of the party!");
+  });
+
+  it("should have a static method makeEveryoneLaugh that returns 'LOL'", () => {
+    expect(Chuckles.makeEveryoneLaugh()).toEqual("LOL");
+  });
+
+  it("should have a static method jokeCount that returns the number of jokes", () => {
+    chuckles.addJoke("Why did the tomato turn red? Because it saw the salad dressing!");
+    chuckles.addJoke("Why did the scarecrow win an award? Because he was outstanding in his field!");
+    expect(Chuckles.jokeCount()).toEqual(2);
+  });
+});
