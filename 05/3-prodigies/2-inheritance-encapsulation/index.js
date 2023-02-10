c2914654ab25d5683bb3b5a52493e9836a46296a
Preston Wallace <wallace.preston@gmail.com>
2023-02-10 13:21:48 -0800

    inheritance and encapsulation challenges

---
+++ 05/3-prodigies/2-inheritance-encapsulation/index.js
@@ -0,0 +1,33 @@
+/* 
+
+The Great Galactic Pie Eating Contest
+
+Contestant Class
+Create a `Contestant` class with the following properties:
+- name (string)
+- pieEaten (number)
+And the following methods:
+- eatPie(amount): takes in an amount and adds it to the pieEaten property.
+- isFull(): returns true if pieEaten is greater than or equal to 10, and false otherwise.
+
+
+Champion Class
+Create a Champion class that extends the Contestant class and has the following additional properties:
+- pieEatingRecords (array)
+- winningPies (number)
+And the following methods:
+- eatPie(amount): if pieEaten plus amount is less than or equal to 20, calls the eatPie method from the Contestant class, and adds the amount to the pieEatingRecords array.
+- setWinningPies(amount): sets the winningPies property to the amount.
+
+Have fun and may the best chef win!
+
+*/
+
+export class Contestant {
+
+}
+
+// hmmm something's missing in this class declaration...
+export class Champion {
+
+}
