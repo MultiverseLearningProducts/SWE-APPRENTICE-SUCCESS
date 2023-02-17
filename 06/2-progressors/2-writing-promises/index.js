cd4bcae7cd9ee79ef83e7d9347cd7c70e860e1a1
Preston Wallace <wallace.preston@gmail.com>
2023-02-17 10:45:48 -0800

    w6 promises: add prompt and tests

---
+++ 06/2-progressors/2-writing-promises/index.js
@@ -0,0 +1,31 @@
+/* 
+
+Write a function, `alligatorPromise` that takes no parameters and returns a Promise.
+
+The function should generate a random integer between 1 and 5, to be used in the Promise that is returned.
+
+The Promise should...
+- Resolve with the string "Alligator!" after a random amount of time between 1 and 5 seconds has passed. 
+- However, if the random time is divisible by 2, the Promise should reject with the error "Oh no, it's a crocodile!" instead.
+
+*/
+
+export const alligatorPromise = () => {
+  // your code here
+
+};
+
+/* 
+
+Write a function `averageNumbers` that takes an array of numbers as input and returns a Promise.
+
+The Promise should... 
+- Resolve with the average of the numbers in the array. 
+- However, if the array contains any negative numbers, the Promise should reject with the error message "Negative numbers are not allowed".
+
+*/
+
+export const averageNumbers = () => {
+  // your code here
+
+};
---
+++ 06/2-progressors/2-writing-promises/index.test.js
@@ -0,0 +1,34 @@
+import { alligatorPromise, averageNumbers } from "./index.js";
+
+describe('Promises', () => {
+  describe("alligatorPromise", () => {
+    test("resolves with 'Alligator!' after a random time between 1 and 5 seconds", async () => {
+      const result = await alligatorPromise();
+      expect(result).toBe("Alligator!");
+    });
+  
+    test("rejects with 'Oh no, it's a crocodile!' if the random time is divisible by 2", async () => {
+      try {
+        await alligatorPromise();
+      } catch (error) {
+        expect(error).toBe("Oh no, it's a crocodile!");
+      }
+    });
+  });
+
+  describe("averageNumbers", () => {
+    test("resolves with the average of the numbers in the array", async () => {
+      const result = await averageNumbers([1, 2, 3, 4, 5]);
+      expect(result).toBe(3);
+    });
+  
+    test("rejects with the error message 'Negative numbers are not allowed'", async () => {
+      try {
+        await averageNumbers([1, 2, -3, 4, 5]);
+      } catch (error) {
+        expect(error).toBe("Negative numbers are not allowed");
+      }
+    });
+  });  
+});
+
