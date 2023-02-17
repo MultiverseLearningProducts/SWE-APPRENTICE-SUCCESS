7ef24aeeb46aa816b6e9f05ac2284c8ff3b3fea7
Preston Wallace <wallace.preston@gmail.com>
2023-02-17 10:56:43 -0800

    w6: edit promises tests

--- 06/2-progressors/2-writing-promises/index.js
+++ 06/2-progressors/2-writing-promises/index.js
@@ -2,10 +2,10 @@
 
 Write a function, `alligatorPromise` that takes no parameters and returns a Promise.
 
-The function should generate a random integer between 1 and 5, to be used in the Promise that is returned.
+The function should generate a random integer between 1 and 4, to be used in the Promise that is returned.
 
 The Promise should...
-- Resolve with the string "Alligator!" after a random amount of time between 1 and 5 seconds has passed. 
+- Resolve with the string "Alligator!" after a random amount of time between 1 and 4 seconds has passed. 
 - However, if the random time is divisible by 2, the Promise should reject with the error "Oh no, it's a crocodile!" instead.
 
 */
