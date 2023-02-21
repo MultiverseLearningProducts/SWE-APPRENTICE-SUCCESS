79fbc7aebf85c824e991cdc956bd223f10482308
stevenjlance <steven.j.lance@gmail.com>
2023-02-21 16:18:47 -0500

    practice folders created

---
+++ 08/2-progressors/1-Controlled-Inputs/Practice/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// Import components here
+
+
+function App() {
+    return(
+        <div className = "page">
+            <h1 id = "title">Controlled Inputs</h1>
+            <div className="app">
+                {/* Render all components here */}
+
+
+
+
+
+            </div>
+        </div>
+        
+    )
+}
+
+module.exports = App;
