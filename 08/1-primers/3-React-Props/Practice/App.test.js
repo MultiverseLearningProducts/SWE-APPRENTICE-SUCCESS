0011326ccdc63b768883ebe1a8546af53aaf2b59
stevenjlance <steven.j.lance@gmail.com>
2023-02-27 17:35:46 -0500

    props practice tests created

--- 08/1-primers/3-React-Props/Practice/App.test.js
+++ 08/1-primers/3-React-Props/Practice/App.test.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App  from './App';
+import Card  from './components/Card';
+import bella from "./components/images/Bella.jpg"
+import bailey from "./components/images/Bailey.jpg"
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 
-// Setup test
-test("App component Renders", () => {
-    const component = renderer.create(<App/>);
-    const instance = component.root;
 
-    const h1 = instance.findByType("h1");
-    expect(h1.children.includes('Dogbook 🐶')).toBe(true);
+test('Card renders with name in a h4', () => {
+    const {root: bellaInstance} = renderer.create(<Card name="Bella" activity = "running" image = {bella}/>);
+    // assert that bolded element includes 'Bella'
+    expect(bellaInstance.findByType("b").children.join('')).toBe('Bella');
+
+    const {root: baileyInstance} = renderer.create(<Card name="Bailey" activity = "long walks" image = {bailey}/>);
+    // assert that bolded element includes 'Bailey'
+    expect(baileyInstance.findByType("b").children.join('')).toBe('Bailey');
+})
+
+test('Card renders with activity in the string "I really love ACTIVITY"', () => {
+    const {root: bellaInstance} = renderer.create(<Card name="Bella" activity = "running" image = {bella}/>);
+    // assert that p includes 'I really love running'
+    expect(bellaInstance.findByType("p").children.join('')).toBe('I really love running');
+
+    const {root: baileyInstance} = renderer.create(<Card name="Bailey" activity = "long walks" image = {bailey}/>);
+    // assert that p includes 'I really love long walks'
+    expect(baileyInstance.findByType("p").children.join('')).toBe('I really love long walks');
+})
+
+
+test('Card renders image with name as alt text"', () => {
+    render(<Card name="Bella" activity = "running" image = {bella}/>);
+    const logo = screen.getByRole('img');
+    expect(logo).toHaveAttribute('alt', 'Bella')
 })
