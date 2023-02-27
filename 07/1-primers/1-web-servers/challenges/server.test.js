7768e3f65980ba9360a5fcd98c2d4a3793ce5429
Preston Wallace <wallace.preston@gmail.com>
2023-02-27 15:54:18 -0800

    make web server tests more granular

--- 07/1-primers/1-web-servers/challenges/server.test.js
+++ 07/1-primers/1-web-servers/challenges/server.test.js
@@ -2,12 +2,27 @@ import request from 'supertest';
 import app from './server';
 
 describe('GET /', () => {
-  it('should return the welcome message and links to the creatures', (done) => {
+
+  it('should return a 200 status code', (done) => {
     request(app)
       .get('/')
-      .expect(200)
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(/Welcome to the Fantasy Creatures Collection!/)
+      .expect(200, done);
+  });
+  it('should return HTML', (done) => {
+    request(app)
+      .get('/')
+      .expect('Content-Type', 'text/html; charset=utf-8', done);
+  });
+
+  it('should return the welcome message', (done) => {
+    request(app)
+      .get('/')
+      .expect(/Welcome to the Fantasy Creatures Collection!/, done);
+  });
+
+  it('should return links to the creatures', (done) => {
+    request(app)
+      .get('/')
       .expect(/<a href="\/dragons.html">Dragons<\/a>/)
       .expect(/<a href="\/unicorns.html">Unicorns<\/a>/)
       .expect(/<a href="\/griffins.html">Griffins<\/a>/)
