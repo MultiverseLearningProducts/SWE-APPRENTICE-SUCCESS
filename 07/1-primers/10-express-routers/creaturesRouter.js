a18ab96c21a94eaf7e421be7211cc0080f12a28a
Preston Wallace <wallace.preston@gmail.com>
2023-03-03 10:25:44 -0800

    w7 express router

--- 07/1-primers/10-express-routers/creaturesRouter.js
+++ 07/1-primers/10-express-routers/creaturesRouter.js
@@ -1,3 +1,6 @@
+// leave this import
+import creatures from './db';
+
 // 👇👇👇 STEP 2: YOUR CODE for creating `/creatures` router, and paste the routes from `app.js` 👇👇👇
 
 // import express
--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,5 @@ export default {
         '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$': '<rootDir>/__mocks__/fileMock.cjs',
       },
   testEnvironment: 'jest-environment-jsdom',
-  testTimeout: 350,
+  testTimeout: 500,
 }
